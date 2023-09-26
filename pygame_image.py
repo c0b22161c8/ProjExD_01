83b42a970fb8f2a638fce9ce4059723adf9ecbfa
yoshimoto <c0b22161c8@edu.teu.ac.jp>
2023-09-26 16:17:37 +0900

    image3

--- pygame_image.py
+++ pygame_image.py
@@ -7,23 +7,25 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg1_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    bg2_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg2_img = pg.transform.flip(bg1_img, True, False)
     ko1_img = pg.image.load("ex01/fig/3.png")
     ko1_img = pg.transform.flip(ko1_img, True, False)
     ko2_img = pg.transform.rotozoom(ko1_img, 10, 1.0)
-    ko_li = [ko1_img, ko2_img]
+    ko3_img = pg.transform.rotozoom(ko1_img, 5, 1.0)
+    ko_li = [ko1_img, ko3_img, ko2_img, ko3_img]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
-        screen.blit(bg1_img, [0-x, 0])
-        screen.blit(bg2_img, [1600-x, 0])
-        screen.blit(ko_li[tmr%2], [300, 200])
+        x = 0
+        y = 1600
+        screen.blit(bg1_img, [x-(tmr%1600), 0])
+        screen.blit(bg2_img, [y-(tmr%1600), 0])
+        screen.blit(ko_li[tmr%4], [300, 200])
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
