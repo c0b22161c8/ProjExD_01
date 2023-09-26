1ea75eb5ab80ff65a7dd0fdcf911c663b2a2bd01
yoshimoto <c0b22161c8@edu.teu.ac.jp>
2023-09-26 15:05:15 +0900

    image1

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    ko1_img = pg.image.load("3.png")
+    ko1_img = pg.transform.flip(ko1_img, True, False)
+    ko2_img = pg.transform.rotozoom(ko1_img, 10, 1.0)
+    ko_li = [ko1_img, ko2_img]
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        pg.display.update()
+        tmr += 1
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
